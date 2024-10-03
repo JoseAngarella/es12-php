2178ba07c77ba49a887f6eb1490ab62016643f1f
Benncio09 <lorebencini13@outlook.com>
2024-10-03 09:02:11 +0200

    finito e funzionante

--- esercizio12.php
+++ esercizio12.php
@@ -6,6 +6,38 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php
+        function scegliColore($inizialeColore){
+            switch($inizialeColore){
+                case "b":
+                    $colore="blue";
+                    break;
+                case "w":
+                    $colore="white";
+                    break;
+            
+                case "r":
+                    $colore="red";
+                    break;
+                case "y":
+                    $colore="yellow";
+                    break;
+                case "g":
+                    $colore="green";
+                    break;
+                default:
+                    $colore="black";
+                    break;
+
+            }
+            return $colore;
+
+        }
+        $lettera="g";
+        $colore=scegliColore("g");
+        echo "<p style='color:$colore'>Questo paragrafo ha la proprietà color con valore $colore</p>";
+    ?>
     
 </body>
 </html>
